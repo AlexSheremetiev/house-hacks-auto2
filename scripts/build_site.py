94d2124e8fbf85fe467c01cf1883e64d66b9a9c9
AlexSheremetiev <alexsheremetiev@icloud.com>
2025-06-04 15:50:34 +0300

    Update build_site.py

--- scripts/build_site.py
+++ scripts/build_site.py
@@ -1,6 +1,6 @@
 import pathlib
 from datetime import datetime, timezone
-import email.utils as eut      # для красивого pubDate
+import email.utils as eut          # для красивого pubDate в RFC 1123
 import markdown
 from feedgen.feed import FeedGenerator
 
@@ -41,16 +41,23 @@ fg.link(href='http://proste-hack.ru/', rel='alternate')
 fg.description('Автолайфхаки и бытовые трюки')
 fg.language('ru')
 
+# объявляем namespace, требуемый Яндекс.Дзеном
+fg._nsmap['yandex'] = 'http://news.yandex.ru'
+
 for md_path in sorted(root.glob('*.md'), reverse=True):
     full = md_path.read_text(encoding='utf-8')
 
-    fe = fg.add_entry()
+    fe = fg.add_entry()                                   # 1) создаём элемент
     fe.title(md_path.stem)
     fe.link(href=f'http://proste-hack.ru/{md_path.stem}.html')
-    fe.description(full[:500])                          # анонс ≤ 500 симв
+    fe.description(full[:500])                            # анонс ≤ 500 симв.
     fe.guid(f'http://proste-hack.ru/{md_path.stem}.html', permalink=True)
-    fe.element('yandex:full-text', full)                # полный текст
+
+    # полный текст — кастомный тег <yandex:full-text>
+    fe._element('yandex:full-text').text = full           # 2) добавляем тег
+
+    # дата публикации в GMT, как просит Дзен
     fe.pubDate(eut.format_datetime(datetime.now(timezone.utc)))
 
-# сохраняем RSS как site/feed.xml
+# ---------- 4. сохраняем RSS-фид ----------
 fg.rss_file(root / 'feed.xml')
